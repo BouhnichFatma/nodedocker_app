ac4b059bbf6408d3a68dca025153d56419d89df7
BouhnichFatma <73854154+BouhnichFatma@users.noreply.github.com>
2021-03-17 11:51:09 +0100

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.get('/',(req,res,next) => {
 
-	res.send('Hello from Node Js using Nodemon <br> hello again');
+	res.send('Hello from Node Js using Nodemon <br> welcome');
 });
 
 router.get('/home', (req, res, next) =>{
